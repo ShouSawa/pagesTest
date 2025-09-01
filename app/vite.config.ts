17648038a206f6f8617a23ca38cfa351171a6d20
Shou <ShotaMain1211@gmail.com>
2025-09-01 14:13:56 +0900

    add deploy.yml & fix package.json and vite.config.ts

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.GITHUB_PAGES
-    ? "pagesTest"
-    : "./",
+  base: '/pagesTest/',
   plugins: [react()],
 })
